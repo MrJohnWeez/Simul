af669e508095d17b6a834658a4d7c0699fb651f7
MrJohnWeez <johnwakeboarding@gmail.com>
2019-06-09 17:50:34 -0400

    Added controller support, Removed updating player normals and box normals, Player can now jump

--- Unity_Project/Simul/Assets/Scrips/PlayerControllers/PlayerController.cs
+++ Unity_Project/Simul/Assets/Scrips/PlayerControllers/PlayerController.cs
@@ -2,95 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary> Make a player able to move around a 3d cube </summary>
 public class PlayerController : MonoBehaviour
 {
-    public LayerMask toHitGround;
+    public LayerMask layersToJumpOn;
     public GameObject cameraPlaceholder = null;
     public float moveSpeed = 7000;
-    public float rotationSpeed = 100;
-    public float gravityRotationSmoothing = 0.5f;
+    public float jumpHeight = 1;
     public bool isActive = false;
+    public float jumpGravity = 9.8f;
 
     private Rigidbody rb = null;
-    private float gravity = -20;
-    private float moveRot = 0;
-    private float movePos = 0;
-    private Quaternion oldRotation;
-    private float lookAngle;
+    private float moveHorizontal = 0;
+    private float moveVertical = 0;
     Vector3 forwardMotion;
     Vector3 rightMotion;
     Vector3 motion;
-
+    private bool isGrounded = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        oldRotation = transform.rotation;
     }
 
     private void Update()
     {
-        moveRot = Input.GetAxisRaw("Horizontal");
-        movePos = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(moveRot, 0, movePos).normalized;
-        // moveRot = direction.x;
-        // movePos = direction.z;
+        //Cursor.visible = false;
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // If using keyboard make sure values are clamped to a magnitude of 1
+        if(direction.magnitude > 1) {
+            direction = direction.normalized;
+        }
         
-        forwardMotion = movePos * Vector3.ProjectOnPlane(cameraPlaceholder.transform.forward, transform.up).normalized;
-        rightMotion = moveRot * Vector3.ProjectOnPlane(cameraPlaceholder.transform.right, transform.up).normalized;
+        // Move the character realitive to the camera 
+        forwardMotion = moveVertical * Vector3.ProjectOnPlane(cameraPlaceholder.transform.forward, transform.up).normalized;
+        rightMotion = moveHorizontal * Vector3.ProjectOnPlane(cameraPlaceholder.transform.right, transform.up).normalized;
         motion = forwardMotion + rightMotion;
 
-        // Quaternion newRotation = Quaternion.LookRotation(forwardMotion + rightMotion);
-
-        // transform.rotation = newRotation;
-        if(direction != Vector3.zero){
+        // Aligh character in the direction of travel
+        if(direction != Vector3.zero) {
             transform.LookAt(transform.position + motion, transform.up);
         }
 
-        oldRotation = transform.rotation;
-        UpdatePlayerNormal();
+        RaycastHit hit;
+        isGrounded = Physics.Raycast(transform.position + new Vector3(0,0.1f,0), -transform.up, out hit, 0.2f, layersToJumpOn);
     }
 
     private void FixedUpdate() {
         if(isActive)
         {
-            // Update position and rotation of player
             rb.AddForce(motion * moveSpeed, ForceMode.Force);
-            // rb.AddRelativeTorque();
+            if(!isGrounded)
+                rb.AddForce(new Vector3(0,-jumpGravity, 0), ForceMode.Acceleration);
+    
+            if(isGrounded && Input.GetButtonDown("Jump"))
+                rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+            
         }
-        rb.AddRelativeForce(Vector3.down * -gravity, ForceMode.Acceleration);
-    }
-
-    /// <summary> Determine if the user is about to fall off a edge and update thier normal to match
-    /// surrounding faces </summary>
-    private void UpdatePlayerNormal()
-    {
-        
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position + new Vector3(0,0.1f,0), -transform.up, out hit, 20f, toHitGround))
-        {
-            // Plane is under user
-            CalculateNewRotation(hit.normal);
-        }
-        else if(Physics.Raycast(transform.position, -transform.forward, out hit, 20f, toHitGround))
-        {
-            // Plane behind user
-            CalculateNewRotation(hit.normal);
-        }
-        else if(Physics.Raycast(transform.position, transform.forward, out hit, 20f, toHitGround))
-        {
-            // Plane infront of user
-            CalculateNewRotation(hit.normal);
-        }
-    }
-
-    /// <summary> Rotate the player so that the player's up vector matches with the normal given
-    /// also mostly keeping the original forward direction </summary>
-    private void CalculateNewRotation(Vector3 hitNormal)
-    {
-        Vector3 myForward = Vector3.Cross(transform.right,hitNormal);
-        Quaternion targetRot = Quaternion.LookRotation(myForward, hitNormal);
-        transform.rotation = Quaternion.Lerp(targetRot, oldRotation, gravityRotationSmoothing);
     }
 }
