ea5138be95be7faa1f77da414e6460723d2b0b5e
wmhii <wmalcolmh@gmail.com>
2019-06-16 18:58:54 -0500

    Added to level 1 and 1.5

--- Unity_Project/Simul/Assets/Scrips/PressureGate.cs
+++ Unity_Project/Simul/Assets/Scrips/PressureGate.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 /// <summary> Allows functions to get ran when a pressure plate is trigged </summary>
 public class PressureGate : MonoBehaviour
 {
+    private bool startingActiveState;
+
     /// <summary> Gate should no longer be blocking path </summary>
     public void DisableGate()
     {
-        gameObject.SetActive(false);
+        gameObject.SetActive(!gameObject.activeSelf);
     }
 
     /// <summary> Gate should block path again </summary>
     public void EnableGate()
     {
-        gameObject.SetActive(true);
+        gameObject.SetActive(!gameObject.activeSelf);
     }
 }
