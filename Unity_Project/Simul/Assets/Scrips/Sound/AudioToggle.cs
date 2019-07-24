cc1cf90483e2a0e7ab5a096fd832ad8a4269003a
MrJohnWeez <johnwakeboarding@gmail.com>
2019-07-24 16:33:37 -0400

    Added songs and fixed a few bugs

---
+++ Unity_Project/Simul/Assets/Scrips/Sound/AudioToggle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioToggle : MonoBehaviour
+{
+    private List<AudioSource> audios = new List<AudioSource>();
+
+    void Start()
+    {
+        GameObject[] possibleAudioSources = GameObject.FindGameObjectsWithTag("BGMusic");
+
+        foreach(GameObject go in possibleAudioSources)
+        {
+            AudioSource aud = go.GetComponent<AudioSource>();
+            if(aud)
+            {
+                audios.Add(aud);
+            }
+        }
+        ForceUpdate();
+    }
+
+    public void ForceUpdate()
+    {
+        if(!SoundControl.SountIsOn)
+        {
+            foreach(AudioSource aud in audios)
+            {
+                aud.Pause();
+            }
+        }
+        else
+        {
+            foreach(AudioSource aud in audios)
+            {
+                aud.Play();
+            }
+        }
+    }
+}
---
+++ Unity_Project/Simul/Assets/Scrips/Sound/SoundControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundControl : MonoBehaviour
+{
+    public static bool SountIsOn = true;
+    public Sprite soundOn = null;
+    public Sprite soundOff = null;
+    public Image soundImage = null;
+    void Start()
+    {
+        UpdateImage();
+    }
+
+
+    public void ToggleSound()
+    {
+        SountIsOn = !SountIsOn;
+        UpdateImage();
+    }
+
+    public void UpdateImage()
+    {
+        if(SoundControl.SountIsOn)
+        {
+            soundImage.sprite = soundOn;
+        }
+        else
+        {
+            soundImage.sprite = soundOff;
+        }
+    }
+}
